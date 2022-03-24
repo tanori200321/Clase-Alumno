2066c75919d8f8c9e7d850b399e1c7c867a19465
SebastianGtr <tanoriruizs@gmail.com>
2022-03-23 23:02:31 -0700

    Agregar archivos de proyecto.

---
+++ EjemplosEstructuraDatos/Alumno.cs
@@ -0,0 +1,75 @@
+﻿namespace EjemplosEstructuraDatos
+{
+    public class Alumno
+    {
+        private string matricula;
+        private string apellidoPaterno;
+        private string apellidoMaterno;
+        private string nombres;
+        private DateTime fechaNacimiento;
+
+        public string Matricula
+        {
+            get
+            {
+                return matricula;
+            }
+            set
+            {
+                matricula = value;
+            }
+        }
+
+        public string ApellidoPaterno
+        {
+            get
+            {
+                return apellidoPaterno;
+            }
+            set
+            {
+                apellidoPaterno = value;
+            }
+        }
+        public string ApellidoMaterno
+        {
+            get
+            {
+                return apellidoMaterno;
+            }
+            set
+            {
+                apellidoMaterno = value;
+            }
+        }
+        public string Nombres
+        {
+            get
+            {
+                return nombres;
+            }
+            set
+            {
+                nombres = value;
+            }
+        }
+        public DateTime FechaNacimiento
+        {
+            get
+            {
+                return fechaNacimiento;
+            }
+            set
+            {
+                fechaNacimiento = value;
+            }
+        }
+        public string NombreCompleto
+        {
+            get
+            {
+                return nombres + " " + apellidoPaterno + " " + apellidoMaterno;
+            }
+        }
+    }
+}
---
+++ EjemplosEstructuraDatos/Program.cs
@@ -0,0 +1,30 @@
+﻿using EjemplosEstructuraDatos;
+
+List<Alumno> lista = new List<Alumno>();
+
+Alumno alumno1 = new Alumno();
+alumno1.Nombres = "Antonio";
+alumno1.ApellidoPaterno = "Rogel";
+alumno1.Matricula = "123";
+alumno1.FechaNacimiento = DateTime.Now;
+
+
+Alumno alumno2 = new Alumno();
+alumno2.Nombres = "Juan";
+alumno2.ApellidoPaterno = "Lopez";
+alumno2.Matricula = "456";
+alumno2.FechaNacimiento = new DateTime(1980,02,28);
+
+lista.Add(alumno1);
+lista.Add(alumno2);
+
+foreach (Alumno alumnoEnLista in lista) 
+{
+    Console.WriteLine(
+        " Nombre: "
+        + alumnoEnLista.NombreCompleto 
+        + " Matricula: " 
+        + alumnoEnLista.Matricula 
+        + " fecha nacimiento: " 
+        + alumnoEnLista.FechaNacimiento.ToString("dd-MM-yyyy"));
+}
