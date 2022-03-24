3d55b54572f0d0ff77457b62f207ccce5b269c81
SebastianGtr <tanoriruizs@gmail.com>
2022-03-23 23:10:50 -0700

    -

--- EjemplosEstructuraDatos/Program.cs
+++ EjemplosEstructuraDatos/Program.cs
@@ -2,18 +2,19 @@
 
 List<Alumno> lista = new List<Alumno>();
 
+// Alumno 1 //
 Alumno a1 = new Alumno();
 a1.Nombres = "Roberto";
 a1.ApellidoPaterno = "Martinez";
 a1.Matricula = "300";
 a1.FechaNacimiento = DateTime.Now;
-
+// Alumno 2 //
 Alumno a2 = new Alumno();
 a2.Nombres = "Pedro";
 a2.ApellidoPaterno = "Aguilar";
 a2.Matricula = "301";
 a2.FechaNacimiento = new DateTime(1980,02,28);
-
+// Alumno 3 //
 Alumno a3 = new Alumno();
 a3.Nombres = "Sebastian";
 a3.ApellidoPaterno = "Tanori";
@@ -23,8 +24,6 @@ a3.FechaNacimiento = new DateTime(2003, 09, 01);
 lista.Add(a1);
 lista.Add(a2);
 lista.Add(a3);
-
-
 
 
 foreach (Alumno alumnoEnLista in lista) 
